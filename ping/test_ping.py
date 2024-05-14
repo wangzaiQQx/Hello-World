f0db428a77b69950781b21a34cc0eaf97d509d11
david <2503348218@qq.com>
2024-05-14 10:02:22 +0800

    a tool testing ping

---
+++ ping/test_ping.py
@@ -0,0 +1,11 @@
+from ping3 import ping
+import concurrent.futures
+
+ip_list = [ip.strip() for ip in open("ping.txt", 'r')]
+
+def ping_test(ip):
+    with open('result.csv', 'a') as f:
+        f.write(f"{ip},{ping(ip)}\n")
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    executor.map(ping_test, ip_list)
